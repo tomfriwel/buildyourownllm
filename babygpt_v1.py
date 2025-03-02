678660b6249252521999273a63eb09db6ef7e3f4
simpx <simpxx@gmail.com>
2025-03-02 23:59:08 +0800

    comments

--- babygpt_v1.py
+++ babygpt_v1.py
@@ -40,31 +40,29 @@ class BabyGPT(nn.Module):
 
     def __init__(self, vocab_size: int, n_embd: int):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
-        self.lm_head = nn.Linear(n_embd, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd) # 嵌入层，把token映射到n_embd维空间
+        self.lm_head = nn.Linear(n_embd, vocab_size) # 线性层，把n_embd维空间映射到vocab_size维空间，
 
     def forward(self, idx, targets=None):
-        B, T = idx.shape
-        idx = idx[:, -T:]
-        tok_emb = self.token_embedding_table(idx)
-        logits = self.lm_head(tok_emb) # (B,T,C)
+        tok_emb = self.token_embedding_table(idx) # 获得token的嵌入表示 (B,T,n_embd)
+        logits = self.lm_head(tok_emb) # 通过线性层，把embedding结果重新映射回vocab_size维空间 (B,T,vocab_size)
 
-        if targets is None:
+        if targets is None: # 推理场景，不需要计算损失值
             loss = None
         else:
             B, T, C = logits.shape
-            logits = logits.view(B*T, C)
-            targets = targets.view(B*T)
-            loss = F.cross_entropy(logits, targets)
+            logits = logits.view(B*T, C) # 把(B,T,C)的形状转换为(B*T,C)，因为交叉熵损失函数第一个参数只接受二维输入。这个操作并没有丢失信息
+            targets = targets.view(B*T) # 把(B,T)的形状转换为(B*T)，因为交叉熵损失函数第二个参数只接受一维输入。这个操作并没有丢失信息
+            loss = F.cross_entropy(logits, targets) # 计算交叉熵损失
         return logits, loss
 
     def generate(self, idx, max_new_tokens):
         for _ in range(max_new_tokens):
-            logits, loss = self(idx)
-            logits = logits[:, -1, :]
-            probs = F.softmax(logits, dim=-1)
-            idx_next = torch.multinomial(probs, num_samples=1)
-            idx = torch.cat((idx, idx_next), dim=1)
+            logits, _ = self(idx) # logits的形状是(B,T,vocab_size)，每一个token都计算了下一个token的概率
+            logits = logits[:, -1, :] # 实际上我们只需要最后一个token算出来的值
+            probs = F.softmax(logits, dim=-1) # 使用softmax函数算概率分布，这里dim=-1表示对最后一个维度进行softmax
+            idx_next = torch.multinomial(probs, num_samples=1) # 根据概率分布随机采样，这里num_samples=1表示采样一个token
+            idx = torch.cat((idx, idx_next), dim=1) # 把采样的token拼接到序列后面
         return idx
 
 tokenizer = Tokenizer(text)
@@ -115,7 +113,6 @@ for iter in range(max_iters):
 
     tokens_processed += batch_size * block_size
 
-    # every once in a while evaluate the loss on train and val sets
     if iter % eval_interval == 0:
         elapsed = time.time() - start_time
         tokens_per_sec = tokens_processed / elapsed if elapsed > 0 else 0
