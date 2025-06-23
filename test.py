4f409e0e2a411be7502f2a245e86411dcde15cd9
tomfriwel <976932447@qq.com>
2025-06-23 16:50:44 +0800

    重构代码以移除未使用的文件读取逻辑，并添加注意力机制的线性变换实现

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
-with open('ci.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
+import torch
+from torch import nn
 
-chars = sorted(list(set(text)))
+head_size = 16 # 人为定义的注意力维度
+x = torch.randn(1, 8, 16) # 单批次 (B=1), 8个token, 每个token 16维特征
+B, T, C = x.shape # B=1, T=8, C=16
 
-print(set(text))
+x = torch.randn(1, 8, 16)
+print(x.shape)
+print(x)
+key = nn.Linear(C, head_size, bias=False)
+query = nn.Linear(C, head_size, bias=False)
+value = nn.Linear(C, head_size, bias=False)
+k = key(x)
+q = query(x)
+v = value(x)
